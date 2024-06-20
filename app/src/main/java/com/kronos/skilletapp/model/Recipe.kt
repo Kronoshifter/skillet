bfd8f0735043d2779e9725785e1279f029c5fef2
Kronos <Kronoshifter@gmail.com>
2024-06-20 03:22:09 -0500

    created recipe time and recipe source

--- app/src/main/java/com/kronos/skilletapp/model/Recipe.kt
+++ app/src/main/java/com/kronos/skilletapp/model/Recipe.kt
@@ -6,9 +6,23 @@ data class Recipe(
   val cover: String, // cover photo
   val notes: String,
   val servings: Int,
-//  val time: RecipeTime,
-//  val source: RecipeSource,
+  val time: RecipeTime,
+  val source: RecipeSource,
   val ingredients: List<Ingredient>,
 //  val instructions: List<Instruction>,
 //  val equipment: List<Equipment>
+)
+
+data class RecipeTime(
+  val preparation: Int,
+  val cooking: Int,
+) {
+
+  val total: Int
+    get() = preparation + cooking
+}
+
+data class RecipeSource(
+  val name: String,
+  val source: String
 )
