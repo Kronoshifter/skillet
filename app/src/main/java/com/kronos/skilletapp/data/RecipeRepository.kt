ffc50ed18075870936603d69463ad9de2a862bfa
Kronos <Kronoshifter@gmail.com>
2024-08-01 16:25:23 -0500

    fixed test data

--- app/src/main/java/com/kronos/skilletapp/data/RecipeRepository.kt
+++ app/src/main/java/com/kronos/skilletapp/data/RecipeRepository.kt
@@ -121,23 +121,11 @@ class RecipeRepository {
     val instructions = listOf(
       Instruction(
         text = "Cook pasta in a pot of salted boiling water until al dente",
-        ingredients = listOf(
-          Ingredient("Mini Shells Pasta", measurement = Measurement(8.0, MeasurementUnit.Ounce)),
-        )
+        ingredients = ingredients.take(1)
       ),
       Instruction(
         text = "Return pot to stove over medium heat then ass butter and olive oil. Once melted, add garlic then saute until light golden brown, about 30 seconds, being very careful not to burn. Sprinkle in flour then whisk and saute for 1 minute. Slowly pour in chicken broth and milk while whisking until mixture is smooth. Season with salt and pepper then switch to a wooden spoon and stir constantly until mixture is thick and bubbly, 4.-5 minutes.",
-        ingredients = listOf(
-          Ingredient("Butter", measurement = Measurement(1.0, MeasurementUnit.Tablespoon)),
-          Ingredient("Olive Oil", measurement = Measurement(1.0, MeasurementUnit.Tablespoon)),
-          Ingredient(
-            "Garlic",
-            measurement = Measurement(2.0, MeasurementUnit.Custom("clove"))
-          ),
-          Ingredient("Flour", measurement = Measurement(2.0, MeasurementUnit.Tablespoon)),
-          Ingredient("Chicken Broth", measurement = Measurement(0.75, MeasurementUnit.Cup)),
-          Ingredient("Milk", measurement = Measurement(2.5, MeasurementUnit.Cup)),
-        )
+        ingredients = ingredients.slice(1..6)
       ),
       Instruction(
         text = "Remove pot from heat then stir in parmesan cheese, garlic powder, and parsley flakes until smooth. Add cooked pasta then stir to combine. Taste then adjust salt and pepper if necessary, and then serve.",
