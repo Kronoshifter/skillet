ca036512e00f878943a0a3a1b2e309c4d973417f
Kronos <Kronoshifter@gmail.com>
2024-08-27 17:55:38 -0500

    added generic infinite scrolling picker component

---
+++ app/src/main/java/com/kronos/skilletapp/ui/component/Picker.kt
@@ -0,0 +1,141 @@
+package com.kronos.skilletapp.ui.component
+
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
+import com.kronos.skilletapp.ui.theme.SkilletAppTheme
+import com.kronos.skilletapp.utils.modifier.verticalFadingEdge
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+
+@Composable
+fun <T> InfiniteScrollingPicker(
+  options: List<T>,
+  selected: T,
+  onSelect: (T) -> Unit,
+  modifier: Modifier = Modifier,
+  visibleItemCount: Int = 3,
+  itemHeight: Dp = 40.dp,
+  dividerColor: Color = LocalContentColor.current,
+  optionContent: @Composable BoxScope.(T) -> Unit = { Text(text = it.toString()) }
+) {
+  require(visibleItemCount % 2 == 1) {
+    "visibleItemCount must be an odd number"
+  }
+
+  val center = visibleItemCount / 2
+  val scrollCount = Int.MAX_VALUE
+  val scrollCenter = scrollCount / 2
+  val startIndex = scrollCenter - (scrollCenter % options.size) - center + options.indexOf(selected)
+
+  val listState = rememberLazyListState(initialFirstVisibleItemIndex = startIndex)
+  val flingBehavior = rememberSnapFlingBehavior(lazyListState = listState)
+
+  Box(modifier = modifier) {
+    LazyColumn(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      state = listState,
+      flingBehavior = flingBehavior,
+      modifier = Modifier
+        .fillMaxWidth()
+        .height(visibleItemCount * itemHeight)
+        .verticalFadingEdge(),
+    ) {
+      items(scrollCount) { index ->
+        Box(
+          contentAlignment = Alignment.Center,
+          modifier = Modifier.height(itemHeight)
+        ) {
+          optionContent(options[index % options.size])
+        }
+      }
+    }
+
+    HorizontalDivider(
+      modifier = Modifier.offset(y = itemHeight * (center)),
+      color = dividerColor
+    )
+
+    HorizontalDivider(
+      modifier = Modifier.offset(y = itemHeight * (center + 1)),
+      color = dividerColor
+    )
+  }
+
+  LaunchedEffect(listState) {
+    snapshotFlow { listState.firstVisibleItemIndex }
+      .map { index -> options[(index + center) % options.size] }
+      .distinctUntilChanged()
+      .collect { onSelect(it) }
+  }
+}
+
+@Preview
+@Composable
+fun PickerPreview() {
+  val options = 0..10
+  var selected by remember { mutableStateOf(options.first()) }
+
+  SkilletAppTheme {
+    Surface {
+      Column {
+        InfiniteScrollingPicker(
+          options = options.toList(),
+          selected = selected,
+          onSelect = { selected = it },
+          visibleItemCount = 3
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "Selected: $selected")
+      }
+    }
+  }
+}
+
+@Preview
+@Composable
+fun DoublePickerPreview() {
+  SkilletAppTheme {
+    Surface {
+      Column {
+        val numberPickerOptions = (0..10).toList()
+        var numberPickerSelected by remember { mutableIntStateOf(numberPickerOptions.first()) }
+
+        val unitPickerOptions = listOf("Seconds", "Minutes", "Hours")
+        var unitPickerSelected by remember { mutableStateOf(unitPickerOptions.first()) }
+
+        Row(modifier = Modifier) {
+          InfiniteScrollingPicker(
+            options = numberPickerOptions,
+            selected = numberPickerSelected,
+            onSelect = { numberPickerSelected = it },
+            modifier = Modifier.weight(1f)
+          )
+
+          InfiniteScrollingPicker(
+            options = unitPickerOptions,
+            selected = unitPickerSelected,
+            onSelect = { unitPickerSelected = it },
+            modifier = Modifier.weight(1f)
+          )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "Selected: $numberPickerSelected $unitPickerSelected")
+      }
+    }
+  }
+}
--- app/src/main/java/com/kronos/skilletapp/utils/modifier/ModifierUtils.kt
+++ app/src/main/java/com/kronos/skilletapp/utils/modifier/ModifierUtils.kt
@@ -1,6 +1,8 @@
 package com.kronos.skilletapp.utils.modifier
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.*
 
 inline fun Modifier.applyIf(condition: Boolean, block: Modifier.() -> Modifier) = if (condition) {
   this then block()
@@ -13,3 +15,32 @@ inline fun Modifier.applyUnless(condition: Boolean, block: Modifier.() -> Modifi
 } else {
   this then block()
 }
+
+fun Modifier.verticalFadingEdge() = this
+  .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+  .drawWithContent {
+    drawContent()
+    drawRect(
+      brush = Brush.verticalGradient(
+        0f to Color.Transparent,
+        0.5f to Color.Black,
+        1f to Color.Transparent
+      ),
+      blendMode = BlendMode.DstIn
+    )
+  }
+
+
+fun Modifier.horizontalFadingEdge() = this
+  .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+  .drawWithContent {
+    drawContent()
+    drawRect(
+      brush = Brush.horizontalGradient(
+        0f to Color.Transparent,
+        0.5f to Color.Black,
+        1f to Color.Transparent
+      ),
+      blendMode = BlendMode.DstIn
+    )
+  }
