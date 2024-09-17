8d1a8092788849387c3d6aac19cb8fbb0f39d001
Kronos <Kronoshifter@gmail.com>
2024-09-17 16:14:42 -0500

    added navigation functions for cooking screen

--- app/src/main/java/com/kronos/skilletapp/SkilletNavigation.kt
+++ app/src/main/java/com/kronos/skilletapp/SkilletNavigation.kt
@@ -7,9 +7,18 @@ sealed interface Route {
   @Serializable data object RecipeList : Route
   @Serializable data class Recipe(val recipeId: String) : Route
   @Serializable data class AddEditRecipe(val title: String, val recipeId: String? = null) : Route
+  @Serializable data class Cooking(val recipeId: String) : Route
 }
 
 class SkilletNavigationActions(private val navController: NavHostController) {
+
+  fun navigateToRecipeList() {
+    navController.navigate(Route.RecipeList) {
+      popUpTo<Route.RecipeList> {
+        inclusive = true
+      }
+    }
+  }
 
   fun navigateToRecipe(recipeId: String) {
     navController.navigate(Route.Recipe(recipeId)) {
@@ -19,5 +28,9 @@ class SkilletNavigationActions(private val navController: NavHostController) {
 
   fun navigateToAddEditRecipe(title: String, recipeId: String? = null) {
     navController.navigate(Route.AddEditRecipe(title, recipeId))
+  }
+
+  fun navigateToCooking(recipeId: String) {
+    navController.navigate(Route.Cooking(recipeId))
   }
 }
