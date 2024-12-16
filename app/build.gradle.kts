f1336cb1edda770b1d0069589ddfe3454ec767fc
Kronos <kronoshifter@gmail.com>
2024-12-16 17:35:45 -0600

    additional android setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,9 @@ android {
   packaging {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
+      excludes += "META-INF/DEPENDENCIES"
     }
+
   }
 
   testOptions {
---
+++ app/src/test/java/com/kronos/skilletapp/RecipeScrapingTests.kt
@@ -0,0 +1,11 @@
+package com.kronos.skilletapp
+
+import io.kotest.core.spec.style.FunSpec
+
+class RecipeScrapingTests : FunSpec({
+  context("Scraping") {
+    test("Pancakes") {
+      
+    }
+  }
+})
