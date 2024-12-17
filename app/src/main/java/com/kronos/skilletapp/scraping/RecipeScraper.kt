00200553dcf7d7bf4351f5499bc47042ddd7808c
Kronos <kronoshifter@gmail.com>
2024-12-17 15:25:47 -0600

    prep for custom serializer

--- app/src/main/java/com/kronos/skilletapp/scraping/RecipeScraper.kt
+++ app/src/main/java/com/kronos/skilletapp/scraping/RecipeScraper.kt
@@ -1,12 +1,14 @@
 package com.kronos.skilletapp.scraping
 
 import com.kronos.skilletapp.model.Recipe
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RecipeHtml(
   val name: String,
+  val description: String = "",
   @SerialName("recipeIngredient") val ingredients: List<String>,
   @SerialName("recipeInstructions") val instructions: List<InstructionHtml>,
   val prepTime: String,
@@ -17,9 +19,6 @@ data class RecipeHtml(
 @Serializable
 data class InstructionHtml(
   val text: String,
-  val name: String? = null,
-  val url: String? = null,
-  val image: String? = null,
 )
 
 class RecipeScraper {
--- app/src/test/java/com/kronos/skilletapp/RecipeScrapingTests.kt
+++ app/src/test/java/com/kronos/skilletapp/RecipeScrapingTests.kt
@@ -1,44 +1,68 @@
 package com.kronos.skilletapp
 
 import android.R.attr.type
+import com.github.michaelbull.result.andThen
+import com.github.michaelbull.result.andThenRecover
+import com.github.michaelbull.result.recoverCatching
+import com.github.michaelbull.result.runCatching
+import com.github.michaelbull.result.unwrap
+import com.kronos.skilletapp.scraping.RecipeHtml
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import it.skrape.core.document
 import it.skrape.core.htmlDocument
 import it.skrape.fetcher.HttpFetcher
 import it.skrape.fetcher.response
 import it.skrape.fetcher.skrape
-import it.skrape.selects.eachText
-import it.skrape.selects.html5.head
 import it.skrape.selects.html5.script
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
 
 class RecipeScrapingTests : FunSpec({
   context("Scraping") {
     val recipeUrl = "https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/"
 
     test("Retrieve JSON-LD") {
-      val extracted = withContext(Dispatchers.IO) {
-        skrape(HttpFetcher) {
-          request {
-            url = recipeUrl
-          }
-
-          response {
-            htmlDocument {
-              relaxed = true
-              script {
-                withAttribute = "type" to "application/ld+json"
-
-                findFirst { html }
-              }
-            }
-          }
-        }
-      }
+      val extracted = extractJsonLd(recipeUrl)
 
       extracted shouldNotBe null
+      extracted shouldNotBe ""
+    }
+
+
+    test("Deserialize JSON-LD") {
+      val format = Json { ignoreUnknownKeys = true }
+
+      val extracted = extractJsonLd(recipeUrl)
+      val json = runCatching {
+        format.decodeFromString<RecipeHtml>(extracted)
+      }.recoverCatching {
+        format.decodeFromString<List<RecipeHtml>>(extracted).first()
+      }.unwrap()
+
+      json shouldNotBe null
+      json.name shouldBe "Good Old-Fashioned Pancakes"
     }
   }
 })
+
+private suspend fun extractJsonLd(recipeUrl: String) = withContext(Dispatchers.IO) {
+  skrape(HttpFetcher) {
+    request {
+      url = recipeUrl
+    }
+
+    response {
+      htmlDocument {
+        relaxed = true
+        script {
+          withAttribute = "type" to "application/ld+json"
+
+          findFirst { html }
+        }
+      }
+    }
+  }
+}
