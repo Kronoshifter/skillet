1fa39b6cf6a2c3d2d28e680e04c3a296d8ec65ef
Kronos <kronoshifter@gmail.com>
2024-12-17 21:01:49 -0600

    added pickFirst for mozilla/public-suffix-list.txt to fix build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ android {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
       excludes += "META-INF/DEPENDENCIES"
+
+      pickFirsts += "mozilla/public-suffix-list.txt"
     }
 
   }
