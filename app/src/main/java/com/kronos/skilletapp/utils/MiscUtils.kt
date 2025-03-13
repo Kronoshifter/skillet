c60c820846f41c8b35f437500f9cb4c77151cf9c
Kronos <kronoshifter@gmail.com>
2025-03-13 15:22:40 -0500

    added utility functions for applying conditionally

--- app/src/main/java/com/kronos/skilletapp/utils/MiscUtils.kt
+++ app/src/main/java/com/kronos/skilletapp/utils/MiscUtils.kt
@@ -7,3 +7,13 @@ import kotlinx.serialization.json.Json
 inline fun <reified T> T.toJson(): String = Json.encodeToString(this)
 
 inline fun <reified T> String.fromJson(): T = Json.decodeFromString(this)
+
+inline fun <T> T.applyIf(condition: Boolean, block: T.() -> Unit): T {
+  if (condition) {
+    block()
+  }
+
+  return this
+}
+
+inline fun <T> T.applyUnless(condition: Boolean, block: T.() -> Unit): T = applyIf(!condition, block)
