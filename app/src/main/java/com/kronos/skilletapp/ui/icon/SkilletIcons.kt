65af2e1b1139d419c3fc868423605fdd979d5db1
Kronos <kronoshifter@gmail.com>
2025-03-12 23:55:33 -0500

    used Valkyrie plugin to generate ImageVector icons to replace drawable resources

---
+++ app/src/main/java/com/kronos/skilletapp/ui/icon/SkilletIcons.kt
@@ -0,0 +1,7 @@
+package com.kronos.skilletapp.ui.icon
+
+object SkilletIcons {
+  object Filled
+
+  object Outlined
+}
---
+++ app/src/main/java/com/kronos/skilletapp/ui/icon/outlined/Skillet.kt
@@ -0,0 +1,93 @@
+package com.kronos.skilletapp.ui.icon.outlined
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.kronos.skilletapp.ui.icon.SkilletIcons
+
+val SkilletIcons.Outlined.Skillet: ImageVector by lazy(LazyThreadSafetyMode.NONE) {
+  ImageVector.Builder(
+    name = "Outlined.Skillet",
+    defaultWidth = 24.dp,
+    defaultHeight = 24.dp,
+    viewportWidth = 960f,
+    viewportHeight = 960f
+  ).apply {
+    path(fill = SolidColor(Color(0xFF000000))) {
+      moveTo(177f, 400f)
+      quadTo(191f, 364f, 181.5f, 336f)
+      quadTo(172f, 308f, 149f, 280f)
+      quadTo(116f, 240f, 105.5f, 204.5f)
+      quadTo(95f, 169f, 102f, 120f)
+      lineTo(180f, 120f)
+      quadTo(172f, 158f, 177.5f, 182f)
+      quadTo(183f, 206f, 206f, 234f)
+      quadTo(244f, 280f, 254.5f, 315.5f)
+      quadTo(265f, 351f, 255f, 400f)
+      lineTo(177f, 400f)
+      close()
+      moveTo(337f, 400f)
+      quadTo(351f, 364f, 342f, 336f)
+      quadTo(333f, 308f, 310f, 280f)
+      quadTo(277f, 240f, 266f, 204.5f)
+      quadTo(255f, 169f, 262f, 120f)
+      lineTo(340f, 120f)
+      quadTo(332f, 158f, 337.5f, 182f)
+      quadTo(343f, 206f, 366f, 234f)
+      quadTo(404f, 280f, 414.5f, 315.5f)
+      quadTo(425f, 351f, 415f, 400f)
+      lineTo(337f, 400f)
+      close()
+      moveTo(497f, 400f)
+      quadTo(511f, 364f, 502f, 336f)
+      quadTo(493f, 308f, 470f, 280f)
+      quadTo(437f, 240f, 426f, 204.5f)
+      quadTo(415f, 169f, 422f, 120f)
+      lineTo(500f, 120f)
+      quadTo(492f, 158f, 497.5f, 182f)
+      quadTo(503f, 206f, 526f, 234f)
+      quadTo(564f, 280f, 574.5f, 315.5f)
+      quadTo(585f, 351f, 575f, 400f)
+      lineTo(497f, 400f)
+      close()
+      moveTo(200f, 800f)
+      quadTo(150f, 800f, 115f, 765f)
+      quadTo(80f, 730f, 80f, 680f)
+      lineTo(80f, 480f)
+      lineTo(641f, 480f)
+      quadTo(646f, 446f, 668f, 420.5f)
+      quadTo(690f, 395f, 722f, 384f)
+      lineTo(907f, 322f)
+      lineTo(932f, 398f)
+      lineTo(747f, 460f)
+      quadTo(735f, 464f, 727.5f, 474.5f)
+      quadTo(720f, 485f, 720f, 498f)
+      lineTo(720f, 680f)
+      quadTo(720f, 730f, 685f, 765f)
+      quadTo(650f, 800f, 600f, 800f)
+      lineTo(200f, 800f)
+      close()
+      moveTo(200f, 720f)
+      lineTo(600f, 720f)
+      quadTo(617f, 720f, 628.5f, 708.5f)
+      quadTo(640f, 697f, 640f, 680f)
+      lineTo(640f, 560f)
+      lineTo(160f, 560f)
+      lineTo(160f, 680f)
+      quadTo(160f, 697f, 171.5f, 708.5f)
+      quadTo(183f, 720f, 200f, 720f)
+      close()
+      moveTo(400f, 640f)
+      quadTo(400f, 640f, 400f, 640f)
+      quadTo(400f, 640f, 400f, 640f)
+      lineTo(400f, 640f)
+      lineTo(400f, 640f)
+      lineTo(400f, 640f)
+      quadTo(400f, 640f, 400f, 640f)
+      quadTo(400f, 640f, 400f, 640f)
+      close()
+    }
+  }.build()
+}
