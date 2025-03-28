de8024689e38f150662206a6d0b42b8be3788771
Kronos <kronoshifter@gmail.com>
2025-03-28 14:34:30 -0500

    tiny adjustment to spacing on recipe card placeholder

--- app/src/main/java/com/kronos/skilletapp/ui/screen/recipelist/RecipeListScreen.kt
+++ app/src/main/java/com/kronos/skilletapp/ui/screen/recipelist/RecipeListScreen.kt
@@ -308,11 +308,10 @@ fun RecipeCard(
           val paint = Paint().apply {
             textSize = 192.sp.toPx()
             typeface = Typeface.DEFAULT
-            textAlign = Paint.Align.CENTER
             color = labelBackgroundColor.copy(alpha = 0.5f).toArgb()
           }
 
-          val x = (size.width * 1f / 4f) + 8.dp.toPx()
+          val x = 8.dp.toPx()
           val y = (size.height * 3f / 4f) + 4.dp.toPx()
 
           canvas.nativeCanvas.drawText(recipe.name.first().uppercase(), x, y, paint)
