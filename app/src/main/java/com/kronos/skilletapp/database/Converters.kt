42958c01cdd56408d2e52997edface8b674133c3
Kronos <kronoshifter@gmail.com>
2025-04-04 06:52:10 -0500

    added ignoreUnknownKeys to recipe typeconverter, so measurements can be deserialized properly

--- app/src/main/java/com/kronos/skilletapp/database/Converters.kt
+++ app/src/main/java/com/kronos/skilletapp/database/Converters.kt
@@ -8,21 +8,23 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class RecipeConverters {
+  private val json = Json { ignoreUnknownKeys = true }
+  
   @TypeConverter
-  fun ingredientsFromJson(value: String): List<Ingredient> = Json.decodeFromString(value)
+  fun ingredientsFromJson(value: String): List<Ingredient> = json.decodeFromString(value)
 
   @TypeConverter
-  fun ingredientsToJson(value: List<Ingredient>): String = Json.encodeToString(value)
+  fun ingredientsToJson(value: List<Ingredient>): String = json.encodeToString(value)
 
   @TypeConverter
-  fun instructionsFromJson(value: String): List<Instruction> = Json.decodeFromString(value)
+  fun instructionsFromJson(value: String): List<Instruction> = json.decodeFromString(value)
 
   @TypeConverter
-  fun instructionsToJson(value: List<Instruction>): String = Json.encodeToString(value)
+  fun instructionsToJson(value: List<Instruction>): String = json.encodeToString(value)
 
   @TypeConverter
-  fun equipmentFromJson(value: String): List<Equipment> = Json.decodeFromString(value)
+  fun equipmentFromJson(value: String): List<Equipment> = json.decodeFromString(value)
 
   @TypeConverter
-  fun equipmentToJson(value: List<Equipment>): String = Json.encodeToString(value)
+  fun equipmentToJson(value: List<Equipment>): String = json.encodeToString(value)
 }
