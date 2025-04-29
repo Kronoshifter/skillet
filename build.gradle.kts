e9135c73e00c6210f17d0123d012db920450b630
Kronos <kronoshifter@gmail.com>
2025-04-29 18:29:54 -0500

    created version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  val kotlin_version = "2.0.21"
-
-  id("com.android.application") version "8.7.2" apply false
-  id("org.jetbrains.kotlin.android") version kotlin_version apply false
-  id("com.google.devtools.ksp") version "$kotlin_version-1.0.27" apply false
-  id("org.jetbrains.kotlin.plugin.compose") version kotlin_version apply false
-  id("androidx.room") version "2.6.1" apply false
-
-  kotlin("plugin.serialization") version kotlin_version
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.ksp) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.androidx.room) apply false
+  alias(libs.plugins.kotlin.serialization) apply false
 }
