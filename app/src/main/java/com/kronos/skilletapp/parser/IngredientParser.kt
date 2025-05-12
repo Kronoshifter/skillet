038c2f7d3d60bde8c175d4e901da081f11d079f4
Kronos <kronoshifter@gmail.com>
2025-05-11 19:31:01 -0500

    added some preprocessing for ingredient parsing, to strip out unicode fraction characters

--- app/src/main/java/com/kronos/skilletapp/parser/IngredientParser.kt
+++ app/src/main/java/com/kronos/skilletapp/parser/IngredientParser.kt
@@ -3,34 +3,66 @@ package com.kronos.skilletapp.parser
 import com.kronos.skilletapp.model.Ingredient
 import com.kronos.skilletapp.parser.grammar.IngredientGrammarLexer
 import com.kronos.skilletapp.parser.grammar.IngredientGrammarParser
+import com.kronos.skilletapp.utils.isNotNullOrBlank
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 
 class IngredientParser {
-
-  fun parseIngredient(text: String): Ingredient = IngredientVisitor().visitIngredient(
+  private val visitor = IngredientVisitor()
+  
+  fun parseIngredient(text: String): Ingredient = visitor.visitIngredient(
     IngredientGrammarParser(
       CommonTokenStream(
         IngredientGrammarLexer(
           CharStreams.fromString(
-            "$text\n"
+            "${preProcess(text)}\n"
           )
         )
       )
     ).ingredient()
   )
 
-  fun parseIngredients(text: String): List<Ingredient> = IngredientVisitor().visitIngredients(
+  fun parseIngredients(text: String): List<Ingredient> = visitor.visitIngredients(
     IngredientGrammarParser(
       CommonTokenStream(
         IngredientGrammarLexer(
           CharStreams.fromString(
-            "$text\n"
+            "${preProcess(text)}\n"
           )
         )
       )
     ).recipe()
   )
+  
+  private fun preProcess(text: String): String {
+    val fractionMap = mapOf(
+      '\u00BC' to "1/4",
+      '\u00BD' to "1/2",
+      '\u00BE' to "3/4",
+      '\u2150' to "1/7",
+      '\u2151' to "1/9",
+      '\u2152' to "1/10",
+      '\u2153' to "1/3",
+      '\u2154' to "2/3",
+      '\u2155' to "1/5",
+      '\u2156' to "2/5",
+      '\u2157' to "3/5",
+      '\u2158' to "4/5",
+      '\u2159' to "1/6",
+      '\u215A' to "5/6",
+      '\u215B' to "1/8",
+      '\u215C' to "3/8",
+      '\u215D' to "5/8",
+      '\u215E' to "7/8"
+    )
+
+    return text.replace("""(\d*)([\u00bc-\u00be\u2150-\u215e])""".toRegex()) { match ->
+      val (digit, fraction) = match.destructured
+      fractionMap[fraction.single()]?.let {
+        if (digit.isNotNullOrBlank()) "$digit $it" else it
+      } ?: match.value
+    }
+  }
 }
 
 //TODO: investigate using Chaquopy to use ingredient-parser-nlp
